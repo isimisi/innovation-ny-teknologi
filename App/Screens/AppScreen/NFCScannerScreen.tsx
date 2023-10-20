df180e682fdc2f73e6531693fb014de72106370d
Isaac <91521821+isimisi@users.noreply.github.com>
2023-10-20 09:51:40 +0200

    removed builds

--- App/Screens/AppScreen/NFCScannerScreen.tsx
+++ App/Screens/AppScreen/NFCScannerScreen.tsx
@@ -1,13 +1,12 @@
 import { SafeAreaView, Text, View } from 'react-native';
 import ScreenLayout from '../../Components/ScreenLayout';
+import Scanner from '../../Components/NFC/Scanner';
 
 // kommende skærm
 export default function NFCScannerScreen() {
    return (
       <ScreenLayout>
-         <View>
-            <Text>NFC</Text>
-         </View>
+         <Scanner />
       </ScreenLayout>
    );
 }
